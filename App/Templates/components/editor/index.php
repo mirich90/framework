cb839be9e807a8b36422c4412ad816e65fd91bd6
genkoskarev <gen.koskarev@gmail.com>
2024-01-07 15:56:31 +0300

    add note

--- App/Templates/components/editor/index.php
+++ App/Templates/components/editor/index.php
@@ -4,5 +4,5 @@ $this->setCss('components/editor/style');
 $this->setJs('components/editor/index');
 ?>
 
-<section id="editor" class="container container-lg ui-card">
+<section id="editor" class="container ui-card">
 </section>
